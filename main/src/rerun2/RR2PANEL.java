d8ac8b8eb7b802be8b11e84ab89df075b41ff514
yum1ru <chan.soo@icloud.com>
2025-08-15 00:39:56 +1000

    Create RR2PANEL.java
    Panel file

---
+++ main/src/rerun2/RR2PANEL.java
@@ -0,0 +1,17 @@
+package rerun2;
+
+import javax.swing.JPanel;
+
+import rerun2.RerunTwo.windowDimensions;
+
+public class RR2PANEL extends JPanel{
+	
+	public static appPanel{
+		
+		JPanel windowPanel = new JPanel();
+		windowPanel.setSize(windowDimensions.appWindowWidth, windowDimensions.appWindowHeight);
+		
+	}
+	
+
+}
